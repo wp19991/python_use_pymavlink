7f6e048a15a9066e1a4a5980d4608a4e40d44bf2
wp-2023 <983214439@qq.com>
2023-11-13 10:37:31 +0800

    使用DroneKit来连接无人机飞控

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # pymavlink
+> 使用python连接并且控制无人机的调研与测试
+
+## DroneKit
+
+- DroneKit是由pymavlink更高一级的抽象而来的，使用DroneKit来控制无人机更加容易
+
+### 示例
+
+- 使用DroneKit的示例：[dronekit_exmaple.py](./dronekit_exmaple.py)
+    - 无人机的连接
+    - 无人机的状态打印
+    - 修改无人机参数
+    - 添加监听回调函数
+
+## pymavlink
 
 > 使用python通过mavlink读取设置无人机的参数（包含pid的参数等）
 
@@ -10,7 +25,7 @@
 - 读取所有参数：[read_all_param.py](./read_all_param.py)
 - 设置参数：[set_param.py](./set_param.py)
 
-## 参考连接
+### 参考连接
 
 - [Pymavlink库 (mavgen)使用指南](https://mavlink.io/zh/mavgen_python/)
 - [MAVLINK通用消息集](https://mavlink.io/zh/messages/common.html)
---
+++ dronekit_exmaple.py
@@ -0,0 +1,76 @@
+import json
+import time
+
+from dronekit import connect
+
+# Connect to the Vehicle (in this case a UDP endpoint)
+# https://dronekit-python.readthedocs.io/en/latest/guide/connecting_vehicle.html
+vehicle = connect('com3', wait_ready=True, baud=57600)
+# 当首次连接到UAV时，DroneKit会下载所有参数（强制参数读取等待,直到下载完成），然后通过监控车辆消息来保持值更新用于对单个参数的更改。
+# 此过程可确保读取支持的参数始终是安全的，并且它们的值将与车辆上的信息相匹配。
+# 可以使用属性（对象）读取、设置、观察和迭代参数。
+
+# 打印系统状态
+print(f"Autopilot Firmware version: {vehicle.version}")
+print(f"Autopilot capabilities (supports ftp): {vehicle.capabilities.ftp}")
+print(f"Global Location: {vehicle.location.global_frame}")
+print(f"Global Location (relative altitude): {vehicle.location.global_relative_frame}")
+print(f"Local Location: {vehicle.location.local_frame}")  # NED
+print(f"Attitude: {vehicle.attitude}")
+print(f"Velocity: {vehicle.velocity}")
+print(f"GPS: {vehicle.gps_0}")
+print(f"Groundspeed: {vehicle.groundspeed}")
+print(f"Airspeed: {vehicle.airspeed}")
+print(f"Gimbal status: {vehicle.gimbal}")
+print(f"Battery: {vehicle.battery}")
+print(f"EKF OK?: {vehicle.ekf_ok}")
+print(f"Last Heartbeat: {vehicle.last_heartbeat}")
+print(f"Rangefinder: {vehicle.rangefinder}")
+print(f"Rangefinder distance: {vehicle.rangefinder.distance}")
+print(f"Rangefinder voltage: {vehicle.rangefinder.voltage}")
+print(f"Heading: {vehicle.heading}")
+print(f"Is Armable?: {vehicle.is_armable}")
+print(f"System status: {vehicle.system_status.state}")
+print(f"Mode: {vehicle.mode.name}")  # settable
+print(f"Armed: {vehicle.armed}")  # settable
+
+# 获取所有参数
+param_map = {}
+for key, value in vehicle.parameters.items():
+    # print(f" Key:{key} Value:{value}")
+    param_map[key] = value
+# 保存所有参数
+with open('param_map.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(param_map, ensure_ascii=False, indent=4))
+
+# 修改参数步骤
+# 更改值的命令不能保证成功，下面的代码片段轮询属性值，以确认它们已更改，然后再继续。
+print(f"change before ATC_RAT_RLL_P={vehicle.parameters['ATC_RAT_RLL_P']}")
+vehicle.parameters['ATC_RAT_RLL_P'] = 0.137
+while not abs(vehicle.parameters['ATC_RAT_RLL_P'] - 0.137) < 0.001:
+    print("changing ...")
+    time.sleep(1)
+print(f"change after ATC_RAT_RLL_P={vehicle.parameters['ATC_RAT_RLL_P']}")
+
+
+# 可以观察某个状态发生的变化，如果变化了，会调用回调函数
+# 回调函数第一个需要是self，第二个是参数名称，第三个是变化的参数值
+def location_callback(self, attr_name, value):
+    print(f"location_callback {attr_name}: {value}", )
+
+
+# Add a callback `location_callback` for the `global_frame` attribute.
+vehicle.add_attribute_listener('*', location_callback)
+# Wait 2s so callback can be notified before the observer is removed
+time.sleep(2)
+# Remove observer - specifying the attribute and previously registered callback function
+vehicle.remove_message_listener('*', location_callback)
+
+
+# 观察所有mavlink的消息，出现变化的时候调用函数
+@vehicle.on_message('*')
+def listener(self, name, message):
+    print(f'mavlink message: {name} : {message}')
+
+
+time.sleep(5)
